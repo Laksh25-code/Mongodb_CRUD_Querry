5e0b3a8a4657cb8cce46ff3ecdf371da3124cd3f
Laksh Patil <lakshpatil574@gmail.com>
2025-11-19 23:42:56 +0530

    Update and Delete querry

---
+++ Mongodb/3_update.mongodb.js
@@ -0,0 +1,22 @@
+use ('ecommers');
+
+
+// db.products.updateOne(
+//     { name: "Laptop"},
+//     { $set: {pricing: 9000}}
+// );
+
+
+// db.products.updateOne(
+//     { name: "Smartphone"},
+//     { $set: {category: "Hardware"}}
+// );
+
+
+
+// db.products.updateMany(
+//     { category: "Electronics"},
+//     { $set: {pricing: 5000}}
+// );
+
+
---
+++ Mongodb/4_delete.mongodb.js
@@ -0,0 +1,15 @@
+use ('ecommers');
+
+// db.contacts.deleteOne(
+//     { name: "Laksh Patil"}
+// )
+
+
+// db.products.deleteMany({ category: "Electronics" })
+
+
+
+// db.products.createIndex({ name: 1 })
+
+// db.products.getIndexes()
+
